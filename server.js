99ae1b00a86dc59416f9a348526a208b12323e80
lewypopescu <popesculewy@gmail.com>
2024-08-31 13:32:49 +0200

    mongodb, mongooase ok

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.Promise = global.Promise;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_HOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
