6a5eb459a5646aee1c5107950452093726d37c55
lewypopescu <popesculewy@gmail.com>
2024-09-02 17:53:14 +0200

    extra patch, favorite ok

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -92,3 +92,28 @@ router.put("/:contactId", async (req, res, next) => {
 });
 
 export default router;
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+
+  if (typeof favorite !== "boolean") {
+    return res.status(400).json({ message: "missing field favorite" });
+  }
+
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite },
+      { new: true }
+    );
+
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+});
